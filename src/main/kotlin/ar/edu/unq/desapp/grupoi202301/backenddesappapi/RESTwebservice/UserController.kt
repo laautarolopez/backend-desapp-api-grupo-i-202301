5a257d00b82443f505cc91c81d7cfe1e89afc758
Thomas Lautaro López <laautarolopez@gmail.com>
2023-04-17 15:21:06 -0300

    UserController /register creado

--- src/main/kotlin/ar/edu/unq/desapp/grupoi202301/backenddesappapi/RESTwebservice/UserController.kt
+++ src/main/kotlin/ar/edu/unq/desapp/grupoi202301/backenddesappapi/RESTwebservice/UserController.kt
@@ -1,16 +1,15 @@
 package ar.edu.unq.desapp.grupoi202301.backenddesappapi.RESTwebservice
 
-import ar.edu.unq.desapp.grupoi202301.backenddesappapi.service.imp.UserService
+import ar.edu.unq.desapp.grupoi202301.backenddesappapi.service.UserService
 import jakarta.transaction.Transactional
 import org.springframework.web.bind.annotation.*
 
-
 @RestController
 @Transactional
-@RequestMapping("/api/crypto")
+@RequestMapping("/api/user")
 class UserController(private val userService: UserService) {
 
     @PostMapping("/register")
-    fun crear(@RequestBody user: UserDTO) = userService.create(user.toModel())
+    fun create(@RequestBody user: UserDTO) = userService.create(user.toModel())
 }
 
