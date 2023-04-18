e544482b2cd7090028249cdd5bba737ce013ccbf
Thomas Lautaro López <laautarolopez@gmail.com>
2023-04-18 17:51:59 -0300

    UserService con @Valid en create

--- src/main/kotlin/ar/edu/unq/desapp/grupoi202301/backenddesappapi/service/UserService.kt
+++ src/main/kotlin/ar/edu/unq/desapp/grupoi202301/backenddesappapi/service/UserService.kt
@@ -1,8 +1,9 @@
 package ar.edu.unq.desapp.grupoi202301.backenddesappapi.service
 
 import ar.edu.unq.desapp.grupoi202301.backenddesappapi.model.User
+import jakarta.validation.Valid
 
 interface UserService {
 
-    fun create(user: User): User
+    fun create(@Valid user: User): User
 }
