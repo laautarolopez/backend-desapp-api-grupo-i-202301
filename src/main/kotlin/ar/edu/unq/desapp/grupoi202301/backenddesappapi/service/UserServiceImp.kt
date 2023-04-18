9d06734233f71dd6fdcc051e0803024cf124b5a8
Thomas Lautaro López <laautarolopez@gmail.com>
2023-04-18 17:52:14 -0300

    UserServiceImp funcionando con create

--- src/main/kotlin/ar/edu/unq/desapp/grupoi202301/backenddesappapi/service/UserServiceImp.kt
+++ src/main/kotlin/ar/edu/unq/desapp/grupoi202301/backenddesappapi/service/UserServiceImp.kt
@@ -3,13 +3,14 @@ package ar.edu.unq.desapp.grupoi202301.backenddesappapi.service
 import ar.edu.unq.desapp.grupoi202301.backenddesappapi.model.User
 import ar.edu.unq.desapp.grupoi202301.backenddesappapi.persistence.UserDAO
 import jakarta.transaction.Transactional
-import org.springframework.beans.factory.annotation.Autowired
+import jakarta.validation.Valid
 import org.springframework.stereotype.Service
+import org.springframework.validation.annotation.Validated
 
 @Service
+@Validated
 @Transactional
 class UserServiceImp(
-    @Autowired
     private val userDAO: UserDAO
     ) : UserService {
 
