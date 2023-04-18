a881dc8e81b9130c487f64dea57a197c3a4cc095
Thomas Lautaro López <laautarolopez@gmail.com>
2023-04-18 18:37:45 -0300

    Crypto creada

---
+++ src/main/kotlin/ar/edu/unq/desapp/grupoi202301/backenddesappapi/model/Crypto.kt
@@ -0,0 +1,29 @@
+package ar.edu.unq.desapp.grupoi202301.backenddesappapi.model
+
+import jakarta.persistence.Column
+import jakarta.persistence.GeneratedValue
+import jakarta.persistence.GenerationType
+import jakarta.persistence.Id
+import jakarta.validation.constraints.Min
+import jakarta.validation.constraints.NotNull
+import org.springframework.format.annotation.DateTimeFormat
+import java.time.LocalDateTime
+
+class Crypto {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    var id: Long? = null
+
+    @Column(nullable = false)
+    var name: CryptoQuoteEnum? = null
+
+    @Column(nullable = false)
+    @NotNull(message = "The time cannot be null.")
+    @DateTimeFormat
+    var time: LocalDateTime? = null
+
+    @Column(nullable = false)
+    @NotNull(message = "The price cannot be less than zero.")
+    var price: Double? = null
+}
