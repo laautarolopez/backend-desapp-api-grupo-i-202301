4b0116fd5c58e9eb9385dd9f970bc62b2501fb2d
Thomas Lautaro López <laautarolopez@gmail.com>
2023-04-18 16:44:58 -0300

    modelo -> User con anotaciones funcionando

--- src/main/kotlin/ar/edu/unq/desapp/grupoi202301/backenddesappapi/model/User.kt
+++ src/main/kotlin/ar/edu/unq/desapp/grupoi202301/backenddesappapi/model/User.kt
@@ -12,12 +12,10 @@ class User {
     var id: Long? = null
 
     @Column(nullable = false)
-    @NotBlank(message = "The name cannot be blank.")
     @Size(min = 3, max = 30, message = "The name must be between 3 and 30 characters long.")
     var name: String? = null
 
     @Column(nullable = false)
-    @NotBlank(message = "The last name cannot be blank.")
     @Size(min = 3, max = 30, message = "The last name must be between 3 and 30 characters long.")
     var lastName: String? = null
 
@@ -27,27 +25,23 @@ class User {
     var email: String? = null
 
     @Column(nullable = false)
-    @NotBlank(message = "The address cannot be blank.")
     @Size(min = 10, max = 30, message = "The address must be between 10 and 30 characters long.")
     var address: String? = null
 
     @Column(nullable = false)
-    @NotBlank(message = "The password cannot be blank.")
     @Size(min = 6, message = "The password must be at least 6 characters long.")
     @Pattern(
-        regexp = "^(?=.*?[a-z])(?=.*?[A-Z])(?=.*?[-+_!@#\$%^&*.,? ])[a-zA-Z0-9-+_!@#\$%^&*.,? ]{6,}$",
+        regexp = "^(?=.*?[a-z])(?=.*?[A-Z])(?=.*?[-+_!@#\$%^&*.,? ])[a-zA-Z0-9-+_!@#\$%^&*.,? ]{0,}$",
         message = "The password must contain at least 1 lower case, 1 upper case and 1 special character."
     )
     var password: String? = null
 
     @Column(nullable = false)
-    @NotBlank(message = "The CVU cannot be blank.")
     @Size(min = 22, max = 22, message = "The CVU must have 22 digits.")
     @Pattern(regexp = "[0-9]+", message = "The CVU must only have digits.")
     var cvuMercadoPago: String? = null
 
     @Column(nullable = false)
-    @NotNull(message = "The wallet address cannot be blank.")
     @Size(min = 8, max = 8, message = "The wallet address must have 8 digits.")
     @Pattern(regexp = "[0-9]+", message = "The wallet address must only have digits.")
     var walletAddress: String? = null
