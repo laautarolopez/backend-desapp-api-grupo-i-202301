209bab7e4dd5c1c55474f64df698283929bc0e25
fabriciomarote <fabrii.cai93@gmail.com>
2023-04-19 00:57:17 -0300

    Se crea TradeBuilder

---
+++ src/main/kotlin/ar/edu/unq/desapp/grupoi202301/backenddesappapi/model/builder/TradeBuilder.kt
@@ -0,0 +1,46 @@
+package ar.edu.unq.desapp.grupoi202301.backenddesappapi.model.builder
+
+import ar.edu.unq.desapp.grupoi202301.backenddesappapi.model.*
+
+class TradeBuilder {
+    var crypto: Crypto? = null
+    var quantity: Double? = null
+    var amountARS: Double? = null
+    var user: String? = null
+    var operation: OperationType? = null
+
+    fun build(): Trade {
+        var trade = Trade()
+        trade.crypto = this.crypto
+        trade.quantity = this.quantity
+        trade.amountARS = this.amountARS
+        trade.user = this.user
+        trade.operation = this.operation
+        return trade
+    }
+
+    fun withCrypto(crypto: Crypto?): TradeBuilder {
+        this.crypto = crypto
+        return this
+    }
+
+    fun withQuantity(quantity: Double?): TradeBuilder {
+        this.quantity = quantity
+        return this
+    }
+
+    fun withAmountARS(amountARS: Double?): TradeBuilder {
+        this.amountARS = amountARS
+        return this
+    }
+
+    fun withUser(user: String?): TradeBuilder {
+        this.user = user
+        return this
+    }
+
+    fun withOperation(operation: OperationType?): TradeBuilder {
+        this.operation = operation
+        return this
+    }
+}
