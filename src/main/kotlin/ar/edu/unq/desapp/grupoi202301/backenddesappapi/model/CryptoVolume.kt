13109e68a974f88a656ee9617fa69471e66cf028
fabriciomarote <fabrii.cai93@gmail.com>
2023-04-19 23:40:10 -0300

    Se modifica CryptoVolume.kt

--- src/main/kotlin/ar/edu/unq/desapp/grupoi202301/backenddesappapi/model/CryptoVolume.kt
+++ src/main/kotlin/ar/edu/unq/desapp/grupoi202301/backenddesappapi/model/CryptoVolume.kt
@@ -1,6 +1,7 @@
 package ar.edu.unq.desapp.grupoi202301.backenddesappapi.model
 
 import jakarta.persistence.*
+import jakarta.validation.constraints.DecimalMin
 import jakarta.validation.constraints.NotNull
 import org.springframework.format.annotation.DateTimeFormat
 import java.time.LocalDateTime
@@ -12,16 +13,18 @@ class CryptoVolume {
     var id: Long? = null
 
     @Column(nullable = false)
-    @NotNull(message = "The time cannot be null.")
+    @NotNull(message = "The time request cannot be null.")
     @DateTimeFormat
     var timeRequest: LocalDateTime? = null
 
     @Column(nullable = false)
     @NotNull(message = "The amount USD cannot be null.")
+    @DecimalMin(value = "0.0", message = "The amount USD cannot be negative.")
     var amountUSD: Double? = null
 
     @Column(nullable = false)
     @NotNull(message = "The amount ARS cannot be null.")
+    @DecimalMin(value = "0.0", message = "The amount ARS cannot be negative.")
     var amountARS: Double? = null
 
     @Column(nullable = false)
