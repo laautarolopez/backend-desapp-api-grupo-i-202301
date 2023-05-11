048a147204238866691eb2c19bfd385e83e0f3de
fabriciomarote <fabrii.cai93@gmail.com>
2023-05-10 22:33:01 -0300

    Se modifica el nombre del archivo a UserPersistence.kt

--- src/main/kotlin/ar/edu/unq/desapp/grupoi202301/backenddesappapi/persistence/UserPersistence.kt
+++ src/main/kotlin/ar/edu/unq/desapp/grupoi202301/backenddesappapi/persistence/UserPersistence.kt
@@ -5,5 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.stereotype.Repository
 
 @Repository
-interface UserDAO : JpaRepository<User, Long> {
+interface UserPersistence : JpaRepository<User, Long> {
 }
