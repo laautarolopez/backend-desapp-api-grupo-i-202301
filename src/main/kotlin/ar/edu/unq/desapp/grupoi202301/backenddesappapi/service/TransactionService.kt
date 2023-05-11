4024ee33d73266a686e932eb241427b8cbcae60b
fabriciomarote <fabrii.cai93@gmail.com>
2023-05-10 22:40:43 -0300

    Se crea TransactionService.kt

---
+++ src/main/kotlin/ar/edu/unq/desapp/grupoi202301/backenddesappapi/service/TransactionService.kt
@@ -0,0 +1,9 @@
+package ar.edu.unq.desapp.grupoi202301.backenddesappapi.service
+
+import ar.edu.unq.desapp.grupoi202301.backenddesappapi.model.Transaction
+import jakarta.validation.Valid
+
+interface TransactionService {
+
+    fun create(@Valid transaction: Transaction): Transaction
+}
