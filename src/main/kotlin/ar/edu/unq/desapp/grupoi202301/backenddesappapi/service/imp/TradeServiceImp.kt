e1ef9b4209f29d03c25ced7893a0bc799155f62a
fabriciomarote <fabrii.cai93@gmail.com>
2023-05-14 23:46:00 -0300

    Se agrega implementacion a TradeServiceImp.kt, para modificar

--- src/main/kotlin/ar/edu/unq/desapp/grupoi202301/backenddesappapi/service/imp/TradeServiceImp.kt
+++ src/main/kotlin/ar/edu/unq/desapp/grupoi202301/backenddesappapi/service/imp/TradeServiceImp.kt
@@ -2,6 +2,7 @@ package ar.edu.unq.desapp.grupoi202301.backenddesappapi.service.imp
 
 import ar.edu.unq.desapp.grupoi202301.backenddesappapi.model.Trade
 import ar.edu.unq.desapp.grupoi202301.backenddesappapi.persistence.TradePersistence
+import ar.edu.unq.desapp.grupoi202301.backenddesappapi.service.CryptoService
 import ar.edu.unq.desapp.grupoi202301.backenddesappapi.service.TradeService
 import ar.edu.unq.desapp.grupoi202301.backenddesappapi.service.UserService
 import jakarta.transaction.Transactional
@@ -13,10 +14,14 @@ import org.springframework.validation.annotation.Validated
 @Transactional
 class TradeServiceImp(
     private val tradePersistence: TradePersistence,
-    private val userService: UserService
+    private val cryptoService: CryptoService,
+    private val userService: UserService,
     ) : TradeService {
 
     override fun create(trade: Trade): Trade {
+        cryptoService.create(trade.crypto!!)
+        userService.create(trade.user!!)
         return tradePersistence.save(trade)
+        // TODO: Eliminar persistencia de Crypto y Usuario
     }
 }
