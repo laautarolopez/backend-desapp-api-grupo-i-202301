1ecb47e85f433e9e78f4c47f24c19f2dcf178408
Thomas Lautaro López <laautarolopez@gmail.com>
2023-05-15 19:44:21 -0300

    interface BinanceRest creda

---
+++ src/main/kotlin/ar/edu/unq/desapp/grupoi202301/backenddesappapi/restWebService/apiBinance/BinanceRest.kt
@@ -0,0 +1,40 @@
+package ar.edu.unq.desapp.grupoi202301.backenddesappapi.restWebService.apiBinance
+
+import retrofit2.Response
+import retrofit2.http.*
+
+/**
+ * Voy a usar funciones suspendidas, para poder usar código asíncrono. Si no quitar el suspend y cambiar Response<T>
+ * por Call<T>
+ * Aqí van todas las llamadas a la API REST que quiera hacer
+ */
+//interface BinanceRest {
+//
+//    @GET("api/users")
+//    suspend fun getAll(@Query("page") page: Int = 0, @Query("per_page") perPage: Int = 0): Response<GetAllDTO>
+//
+//    @GET("api/users/{id}")
+//    suspend fun getById(@Path("id") id: Int): Response<GetByIdDTO>
+//
+//    @POST("api/users")
+//    suspend fun create(@Body user: User): Response<CreateDTO>
+//
+//    @PUT("api/users/{id}")
+//    suspend fun update(@Path("id") id: Int, @Body user: User): Response<UpdateDTO>
+//
+//    @PATCH("api/users/{id}")
+//    suspend fun upgrade(@Path("id") id: Int, @Body user: User): Response<UpdateDTO>
+//
+//    @DELETE("api/users/{id}")
+//    suspend fun delete(@Path("id") id: Int): Response<Void> // Es void porque no devuelve nada
+//
+//    @POST("api/login")
+//    suspend fun login(@Body user: LoginDTO): Response<TokenDTO>
+//
+//    @GET("api/users")
+//    suspend fun getAllWithToken(
+//            @Header("Authorization") token: String,
+//            @Query("page") page: Int = 0,
+//            @Query("per_page") perPage: Int = 0
+//    ): Response<GetAllDTO>
+//}
