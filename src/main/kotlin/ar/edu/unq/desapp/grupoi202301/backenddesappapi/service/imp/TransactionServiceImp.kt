b7e78fe92729bda5216bf740611336bae06f2e27
fabriciomarote <fabrii.cai93@gmail.com>
2023-05-14 23:46:53 -0300

    Se agrega implementacion a TransactionServiceImp.kt, para modificar

--- src/main/kotlin/ar/edu/unq/desapp/grupoi202301/backenddesappapi/service/imp/TransactionServiceImp.kt
+++ src/main/kotlin/ar/edu/unq/desapp/grupoi202301/backenddesappapi/service/imp/TransactionServiceImp.kt
@@ -1,7 +1,9 @@
 package ar.edu.unq.desapp.grupoi202301.backenddesappapi.service.imp
 
 import ar.edu.unq.desapp.grupoi202301.backenddesappapi.model.Transaction
+import ar.edu.unq.desapp.grupoi202301.backenddesappapi.persistence.TradePersistence
 import ar.edu.unq.desapp.grupoi202301.backenddesappapi.persistence.TransactionPersistence
+import ar.edu.unq.desapp.grupoi202301.backenddesappapi.service.TradeService
 import ar.edu.unq.desapp.grupoi202301.backenddesappapi.service.TransactionService
 import ar.edu.unq.desapp.grupoi202301.backenddesappapi.service.UserService
 import jakarta.transaction.Transactional
@@ -13,10 +15,14 @@ import org.springframework.validation.annotation.Validated
 @Transactional
 class TransactionServiceImp(
     private val transactionPersistence: TransactionPersistence,
-    private val userService: UserService
+    private val userService: UserService,
+    private val tradeService: TradeService,
     ) : TransactionService {
 
     override fun create(transaction: Transaction): Transaction {
+        userService.create(transaction.user!!)
+        tradeService.create(transaction.trade!!)
         return transactionPersistence.save(transaction)
+        // TODO: Eliminar persistencia de Usuario y Trade
     }
 }
