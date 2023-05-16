e38e73d5e3e847b14b6a383cbc7d76785df65ec1
Thomas Lautaro López <laautarolopez@gmail.com>
2023-05-16 19:42:06 -0300

    BinanceService creado con getPrice

---
+++ src/main/kotlin/ar/edu/unq/desapp/grupoi202301/backenddesappapi/restWebService/apiBinance/BinanceService.kt
@@ -0,0 +1,24 @@
+package ar.edu.unq.desapp.grupoi202301.backenddesappapi.restWebService.apiBinance
+
+import ar.edu.unq.desapp.grupoi202301.backenddesappapi.model.CryptoName
+import retrofit2.Call
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+interface BinanceService {
+    @GET("price")
+    fun getPrice(@Query("symbol") cryptoName: CryptoName): Call<PriceResponse>
+
+    companion object {
+        fun create(): BinanceService {
+            val retrofit = Retrofit.Builder()
+                    .baseUrl("https://api.binance.com/api/v3/ticker/")
+                    .addConverterFactory(GsonConverterFactory.create())
+                    .build()
+
+            return retrofit.create(BinanceService::class.java)
+        }
+    }
+}
