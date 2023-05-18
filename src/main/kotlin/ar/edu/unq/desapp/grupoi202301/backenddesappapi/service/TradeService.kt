83bb169528726b9978258f6c494b10db62bcaa10
fabriciomarote <fabrii.cai93@gmail.com>
2023-05-18 14:38:07 -0300

    Se agrega funciones recoverAll y recoverActives a TradeService.kt

--- src/main/kotlin/ar/edu/unq/desapp/grupoi202301/backenddesappapi/service/TradeService.kt
+++ src/main/kotlin/ar/edu/unq/desapp/grupoi202301/backenddesappapi/service/TradeService.kt
@@ -1,9 +1,14 @@
 package ar.edu.unq.desapp.grupoi202301.backenddesappapi.service
 
 import ar.edu.unq.desapp.grupoi202301.backenddesappapi.model.Trade
+import ar.edu.unq.desapp.grupoi202301.backenddesappapi.model.User
 import jakarta.validation.Valid
 
 interface TradeService {
 
     fun create(@Valid trade: Trade): Trade
+
+    fun recoverAll(): List<Trade>
+
+    fun recoverActives(idUser: Long): List<Trade>
 }
