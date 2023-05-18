0b7e0e4aaeffc1470a3d79f7b527d39a0b710311
fabriciomarote <fabrii.cai93@gmail.com>
2023-05-18 14:39:26 -0300

    Se modifica atributos en modelo Transaction.kt

--- src/main/kotlin/ar/edu/unq/desapp/grupoi202301/backenddesappapi/model/Transaction.kt
+++ src/main/kotlin/ar/edu/unq/desapp/grupoi202301/backenddesappapi/model/Transaction.kt
@@ -11,15 +11,7 @@ class Transaction {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     var id: Long? = null
-
-    @NotNull(message = "The crypto cannot be null.")
-    var crypto: CryptoName? = null
-
-    @Column(nullable = false)
-    @NotNull(message = "The quantity cannot be null.")
-    @DecimalMin(value = "0.0", message = "The quantity cannot be negative.")
-    var quantity: Double? = null
-
+    
     @Column(nullable = false)
     @NotNull(message = "The quotation crypto cannot be null.")
     @DecimalMin(value = "0.0", message = "The quotation crypto cannot be negative.")
@@ -29,20 +21,6 @@ class Transaction {
     @NotNull(message = "The amount of operation cannot be null.")
     @DecimalMin(value = "0.0", message = "The amount of operation cannot be negative.")
     var amountOperation: Double? = null
-
-    @ManyToOne(fetch = FetchType.LAZY)
-    @NotNull(message = "The user cannot be null.")
-    var user: User? = null
-
-    @Column(nullable = false)
-    @NotNull(message = "The numbers of operations cannot be null.")
-    @Min(value = 0, message = "The number must be equal to or greater than 0.")
-    var numberOperations: Int? = null
-
-    @Column(nullable = false)
-    @Min(value = 0, message = "The number must be equal to or greater than 0.")
-    @NotNull(message = "The reputation cannot be null.")
-    var reputation: Int? = null
 
     @ManyToOne(fetch = FetchType.LAZY)
     @NotNull(message = "The trade cannot be null.")
