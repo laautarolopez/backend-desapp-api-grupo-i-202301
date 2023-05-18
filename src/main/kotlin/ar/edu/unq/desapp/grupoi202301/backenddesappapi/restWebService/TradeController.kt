fb0ef87818772839331f87abdc971d28a69bb54a
fabriciomarote <fabrii.cai93@gmail.com>
2023-05-18 14:43:19 -0300

    Se crea TradeController

---
+++ src/main/kotlin/ar/edu/unq/desapp/grupoi202301/backenddesappapi/restWebService/TradeController.kt
@@ -0,0 +1,115 @@
+package ar.edu.unq.desapp.grupoi202301.backenddesappapi.restWebService
+
+import ar.edu.unq.desapp.grupoi202301.backenddesappapi.model.Trade
+import ar.edu.unq.desapp.grupoi202301.backenddesappapi.restWebService.DTO.TradeCreateDTO
+import ar.edu.unq.desapp.grupoi202301.backenddesappapi.restWebService.DTO.TradeDTO
+import ar.edu.unq.desapp.grupoi202301.backenddesappapi.restWebService.DTO.TradeResponseDTO
+import ar.edu.unq.desapp.grupoi202301.backenddesappapi.restWebService.exception.ErrorResponseDTO
+import ar.edu.unq.desapp.grupoi202301.backenddesappapi.service.TradeService
+import io.swagger.v3.oas.annotations.Operation
+import io.swagger.v3.oas.annotations.media.ArraySchema
+import io.swagger.v3.oas.annotations.media.Content
+import io.swagger.v3.oas.annotations.media.Schema
+import io.swagger.v3.oas.annotations.responses.ApiResponse
+import io.swagger.v3.oas.annotations.responses.ApiResponses
+import io.swagger.v3.oas.annotations.tags.Tag
+import jakarta.transaction.Transactional
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.*
+
+@RestController
+@Transactional
+@Tag(name = "trades")
+@RequestMapping("trades")
+class TradeController(private val tradeService: TradeService) {
+
+    @Operation(summary = "Create a trade")
+    @ApiResponses(
+        value = [
+            ApiResponse(
+                responseCode = "200",
+                content = [
+                    Content(
+                        mediaType = "application/json",
+                        schema = Schema(implementation = Trade::class)
+                    )
+                ]
+            ),
+            ApiResponse(
+                responseCode = "400",
+                description = "Bad Request",
+                content = [
+                    Content(
+                        mediaType = "application/json",
+                        schema = Schema(implementation = ErrorResponseDTO::class)
+                    )
+                ]
+            )
+        ]
+    )
+    @PostMapping("/create")
+    fun create(@RequestBody trade: TradeCreateDTO) : ResponseEntity<Trade> {
+        val trade = tradeService.create(trade.toModel())
+        return ResponseEntity.ok().body(trade)
+    }
+
+    @Operation(summary = "Get all the active trade of a user")
+    @ApiResponses(
+        value = [
+            ApiResponse(
+                responseCode = "200",
+                content = [
+                    Content(
+                        mediaType = "application/json",
+                        array = ArraySchema(schema = Schema(implementation = TradeResponseDTO::class))
+                    )
+                ]
+            ),
+            ApiResponse(
+                responseCode = "400",
+                description = "Bad Request",
+                content = [
+                    Content(
+                        mediaType = "application/json",
+                        schema = Schema(implementation = ErrorResponseDTO::class)
+                    )
+                ]
+            )
+        ]
+    )
+    @GetMapping("/trades/{idUser}")
+    fun getTradesActive(@PathVariable("idUser") idUser: Long): ResponseEntity<List<TradeResponseDTO>> {
+        val trades = tradeService.recoverActives(idUser).map { trade -> TradeResponseDTO.fromModel(trade) }
+        return ResponseEntity.ok().body(trades)
+    }
+
+    @Operation(summary = "Get all the active trade of a user")
+    @ApiResponses(
+        value = [
+            ApiResponse(
+                responseCode = "200",
+                content = [
+                    Content(
+                        mediaType = "application/json",
+                        array = ArraySchema(schema = Schema(implementation = TradeDTO::class))
+                    )
+                ]
+            ),
+            ApiResponse(
+                responseCode = "400",
+                description = "Bad Request",
+                content = [
+                    Content(
+                        mediaType = "application/json",
+                        schema = Schema(implementation = ErrorResponseDTO::class)
+                    )
+                ]
+            )
+        ]
+    )
+    @GetMapping("/trades")
+    fun getTrades(): ResponseEntity<List<TradeDTO>> {
+        val trades = tradeService.recoverAll().map { trade -> TradeDTO.fromModel(trade) }
+        return ResponseEntity.ok().body(trades)
+    }
+}
