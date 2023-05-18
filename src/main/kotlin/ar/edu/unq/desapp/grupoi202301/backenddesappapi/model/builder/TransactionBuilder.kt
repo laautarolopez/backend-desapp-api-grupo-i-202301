03fbd1dd9b3415e8e772101f91e5576d6f29ae9e
fabriciomarote <fabrii.cai93@gmail.com>
2023-05-18 20:28:21 -0300

    Se elimina atributo quotationCrypto a TransactionBuilder.kt

--- src/main/kotlin/ar/edu/unq/desapp/grupoi202301/backenddesappapi/model/builder/TransactionBuilder.kt
+++ src/main/kotlin/ar/edu/unq/desapp/grupoi202301/backenddesappapi/model/builder/TransactionBuilder.kt
@@ -3,7 +3,6 @@ package ar.edu.unq.desapp.grupoi202301.backenddesappapi.model.builder
 import ar.edu.unq.desapp.grupoi202301.backenddesappapi.model.*
 
 class TransactionBuilder {
-    var quotationCrypto: Double? = null
     var amountOperation: Double? = null
     var trade: Trade? =  null
     var shippingAddress: String? = null
@@ -11,17 +10,11 @@ class TransactionBuilder {
 
     fun build(): Transaction {
         var transaction = Transaction()
-        transaction.quotationCrypto = this.quotationCrypto
         transaction.amountOperation = this.amountOperation
         transaction.trade = this.trade
         transaction.shippingAddress = this.shippingAddress
         transaction.action = this.action
         return transaction
-    }
-
-    fun withQuotationCrypto(quotationCrypto: Double?): TransactionBuilder {
-        this.quotationCrypto = quotationCrypto
-        return this
     }
 
     fun withAmountOperation(amountOperation: Double?): TransactionBuilder {
