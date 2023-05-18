bd19ec1d981a9c047d86801adceccdd51e1c5036
fabriciomarote <fabrii.cai93@gmail.com>
2023-05-18 14:43:36 -0300

    Se crea TradeCreateDTO

---
+++ src/main/kotlin/ar/edu/unq/desapp/grupoi202301/backenddesappapi/restWebService/DTO/TradeCreateDTO.kt
@@ -0,0 +1,51 @@
+package ar.edu.unq.desapp.grupoi202301.backenddesappapi.restWebService.DTO
+
+import ar.edu.unq.desapp.grupoi202301.backenddesappapi.model.OperationType
+import ar.edu.unq.desapp.grupoi202301.backenddesappapi.model.Trade
+import ar.edu.unq.desapp.grupoi202301.backenddesappapi.model.exceptions.OperationEmptyException
+
+class TradeCreateDTO(
+    var crypto: CryptoDTO?,
+    var quantity: Double?,
+    var amountARS: Double?,
+    var user: UserSimpleDTO?,
+    var operation: String?
+) {
+
+    fun toModel(): Trade {
+        val trade = Trade()
+        trade.crypto = this.crypto!!.toModel()
+        trade.quantity = this.quantity
+        trade.amountARS = this.amountARS
+        trade.user = this.user!!.toModel()
+        trade.operation = this.verifyOperation(this.operation)
+        return trade
+    }
+
+    companion object {
+        fun fromModel(trade: Trade) =
+            TradeCreateDTO(
+                crypto = CryptoDTO(
+                    trade.crypto!!.id,
+                    trade.crypto!!.name, /*trade.crypto!!.time,*/
+                    trade.crypto!!.price
+                ),
+                quantity = trade.quantity,
+                amountARS = trade.amountARS,
+                user = UserSimpleDTO(trade.user!!.id, trade.user!!.name, trade.user!!.lastName),
+                operation = trade.operation.toString()
+            )
+    }
+
+    private fun verifyOperation(operation: String?): OperationType {
+        var newOperation: OperationType
+        when (operation) {
+            "BUY" -> newOperation = OperationType.BUY
+            "SALE" -> newOperation = OperationType.SALE
+            else -> {
+                throw OperationEmptyException()
+            }
+        }
+        return newOperation
+    }
+}
