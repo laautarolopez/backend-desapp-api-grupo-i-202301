133827008679bebba9dd96f693aa77dcfdc20daf
fabriciomarote <fabrii.cai93@gmail.com>
2023-05-18 14:37:15 -0300

    Se agrega funcion findAll a TradePersistence.kt

--- src/main/kotlin/ar/edu/unq/desapp/grupoi202301/backenddesappapi/persistence/TradePersistence.kt
+++ src/main/kotlin/ar/edu/unq/desapp/grupoi202301/backenddesappapi/persistence/TradePersistence.kt
@@ -1,8 +1,11 @@
 package ar.edu.unq.desapp.grupoi202301.backenddesappapi.persistence
 
 import ar.edu.unq.desapp.grupoi202301.backenddesappapi.model.Trade
+import ar.edu.unq.desapp.grupoi202301.backenddesappapi.model.User
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.stereotype.Repository
 
 @Repository
-interface TradePersistence : JpaRepository<Trade, Long> { }
+interface TradePersistence : JpaRepository<Trade, Long> {
+    override fun findAll(): List<Trade>
+}
