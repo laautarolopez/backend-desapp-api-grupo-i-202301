dac248862585cbca5f7da72ab0448229e03f355d
Thomas Lautaro López <laautarolopez@gmail.com>
2023-05-17 21:48:00 -0300

    getPrices en CryptoController

--- src/main/kotlin/ar/edu/unq/desapp/grupoi202301/backenddesappapi/restWebService/CryptoController.kt
+++ src/main/kotlin/ar/edu/unq/desapp/grupoi202301/backenddesappapi/restWebService/CryptoController.kt
@@ -1,9 +1,10 @@
 package ar.edu.unq.desapp.grupoi202301.backenddesappapi.restWebService
 
-import ar.edu.unq.desapp.grupoi202301.backenddesappapi.model.CryptoName
+import ar.edu.unq.desapp.grupoi202301.backenddesappapi.restWebService.apiBinance.PriceResponse
 import ar.edu.unq.desapp.grupoi202301.backenddesappapi.restWebService.exception.ErrorResponseDTO
 import ar.edu.unq.desapp.grupoi202301.backenddesappapi.service.CryptoService
 import io.swagger.v3.oas.annotations.Operation
+import io.swagger.v3.oas.annotations.media.ArraySchema
 import io.swagger.v3.oas.annotations.media.Content
 import io.swagger.v3.oas.annotations.media.Schema
 import io.swagger.v3.oas.annotations.responses.ApiResponse
@@ -13,8 +14,6 @@ import jakarta.transaction.Transactional
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
-import org.springframework.web.bind.annotation.PostMapping
-import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
@@ -32,7 +31,7 @@ class CryptoController(private val cryptoService: CryptoService) {
                         content = [
                             Content(
                                     mediaType = "application/json",
-                                    schema = Schema(implementation = Double::class)
+                                    schema = Schema(implementation = PriceResponse::class)
                             )
                         ]
                 ),
@@ -48,9 +47,39 @@ class CryptoController(private val cryptoService: CryptoService) {
                 )
             ]
     )
-    @GetMapping("/{cryptoName}/price")
-    fun getPrice(@PathVariable("cryptoName") cryptoName: CryptoName): ResponseEntity<Double> {
+    @GetMapping("/prices/{cryptoName}")
+    fun getPrice(@PathVariable("cryptoName") cryptoName: String): ResponseEntity<PriceResponse> {
         val price = cryptoService.getPrice(cryptoName)
         return ResponseEntity.ok().body(price)
+    }
+
+    @Operation(summary = "Get prices of cryptos")
+    @ApiResponses(
+        value = [
+            ApiResponse(
+                responseCode = "200",
+                content = [
+                    Content(
+                        mediaType = "application/json",
+                        array = ArraySchema(schema = Schema(implementation = PriceResponse::class))
+                    )
+                ]
+            ),
+            ApiResponse(
+                responseCode = "400",
+                description = "Bad Request",
+                content = [
+                    Content(
+                        mediaType = "application/json",
+                        schema = Schema(implementation = ErrorResponseDTO::class)
+                    )
+                ]
+            )
+        ]
+    )
+    @GetMapping("/prices")
+    fun getPrices(): ResponseEntity<List<PriceResponse>> {
+        val prices = cryptoService.getPrices()
+        return ResponseEntity.ok().body(prices)
     }
 }
