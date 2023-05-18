4c924158f9a18504378543d4f599c7815d2f57b7
Thomas Lautaro López <laautarolopez@gmail.com>
2023-05-17 21:48:23 -0300

    getPrices en CryptoService y CryptoServiceImp

--- src/main/kotlin/ar/edu/unq/desapp/grupoi202301/backenddesappapi/service/CryptoService.kt
+++ src/main/kotlin/ar/edu/unq/desapp/grupoi202301/backenddesappapi/service/CryptoService.kt
@@ -1,12 +1,14 @@
 package ar.edu.unq.desapp.grupoi202301.backenddesappapi.service
 
 import ar.edu.unq.desapp.grupoi202301.backenddesappapi.model.Crypto
-import ar.edu.unq.desapp.grupoi202301.backenddesappapi.model.CryptoName
+import ar.edu.unq.desapp.grupoi202301.backenddesappapi.restWebService.apiBinance.PriceResponse
 import jakarta.validation.Valid
 
 interface CryptoService {
 
     fun create(@Valid crypto: Crypto): Crypto
 
-    fun getPrice(cryptoName: CryptoName): Double
+    fun getPrice(cryptoName: String): PriceResponse
+
+    fun getPrices(): List<PriceResponse>
 }
--- src/main/kotlin/ar/edu/unq/desapp/grupoi202301/backenddesappapi/service/imp/CryptoServiceImp.kt
+++ src/main/kotlin/ar/edu/unq/desapp/grupoi202301/backenddesappapi/service/imp/CryptoServiceImp.kt
@@ -1,9 +1,9 @@
 package ar.edu.unq.desapp.grupoi202301.backenddesappapi.service.imp
 
 import ar.edu.unq.desapp.grupoi202301.backenddesappapi.model.Crypto
-import ar.edu.unq.desapp.grupoi202301.backenddesappapi.model.CryptoName
 import ar.edu.unq.desapp.grupoi202301.backenddesappapi.persistence.CryptoPersistence
 import ar.edu.unq.desapp.grupoi202301.backenddesappapi.restWebService.apiBinance.BinanceResponse
+import ar.edu.unq.desapp.grupoi202301.backenddesappapi.restWebService.apiBinance.PriceResponse
 import ar.edu.unq.desapp.grupoi202301.backenddesappapi.service.CryptoService
 import jakarta.transaction.Transactional
 import org.springframework.stereotype.Service
@@ -20,7 +20,11 @@ class CryptoServiceImp(
         return cryptoPersistence.save(crypto)
     }
 
-    override fun getPrice(cryptoName: CryptoName): Double {
+    override fun getPrice(cryptoName: String): PriceResponse {
         return BinanceResponse().getPrice(cryptoName)
+    }
+
+    override fun getPrices(): List<PriceResponse> {
+        return BinanceResponse().getPrices()
     }
 }
