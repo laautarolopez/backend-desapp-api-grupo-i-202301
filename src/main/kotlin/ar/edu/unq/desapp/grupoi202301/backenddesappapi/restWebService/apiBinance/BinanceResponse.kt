d92796127b5fce91400b8b3868fe7c8fab8eb793
Thomas Lautaro López <laautarolopez@gmail.com>
2023-05-17 21:47:40 -0300

    getPrices en BinanceResponse

--- src/main/kotlin/ar/edu/unq/desapp/grupoi202301/backenddesappapi/restWebService/apiBinance/BinanceResponse.kt
+++ src/main/kotlin/ar/edu/unq/desapp/grupoi202301/backenddesappapi/restWebService/apiBinance/BinanceResponse.kt
@@ -1,33 +1,49 @@
 package ar.edu.unq.desapp.grupoi202301.backenddesappapi.restWebService.apiBinance
 
 import ar.edu.unq.desapp.grupoi202301.backenddesappapi.model.CryptoName
+import ar.edu.unq.desapp.grupoi202301.backenddesappapi.restWebService.exception.BinanceResponseException
+import ar.edu.unq.desapp.grupoi202301.backenddesappapi.restWebService.exception.BinanceServerException
+import ar.edu.unq.desapp.grupoi202301.backenddesappapi.restWebService.exception.ErrorBinanceResponse
+import com.google.gson.Gson
 import retrofit2.Call
-import retrofit2.Callback
-import retrofit2.Response
 
-data class PriceResponse(val symbol: String, val price: Double)
+
+data class PriceResponse(val cryptoName: String, val price: Double)
 
 class BinanceResponse {
-    fun getPrice(cryptoName: CryptoName): Double {
-        val url = "https://api.binance.com/api/v3/ticker/price?symbol=$cryptoName"
+    fun getPrice(cryptoName: String): PriceResponse {
         val binanceService = BinanceService.create()
         val call = binanceService.getPrice(cryptoName)
-        return call.execute().body()!!.price
 
-        call.enqueue(object : Callback<PriceResponse> {
-            override fun onResponse(call: Call<PriceResponse>, response: Response<PriceResponse>) {
-                if (response.isSuccessful) {
-                    val price = response.body()
-                    price!!.price
-                } else {
-                    response.code()
-                }
-            }
+        return executeCall(cryptoName, call)
+    }
 
-            override fun onFailure(call: Call<PriceResponse>, t: Throwable) {
-                t.printStackTrace()
+    private fun executeCall(cryptoName: String, call: Call<PriceResponse>): PriceResponse {
+        var response = call.execute()
+        if(response.isSuccessful) {
+            return PriceResponse(cryptoName, response.body()!!.price)
+        } else {
+            try {
+                val gson = Gson()
+                val error: ErrorBinanceResponse = gson.fromJson(response.errorBody()?.charStream(), ErrorBinanceResponse::class.java)
+                throw BinanceResponseException(cryptoName, error.msg)
+            } catch (e: com.google.gson.JsonSyntaxException) {
+                throw BinanceServerException("The server does not respond.")
             }
-        })
-        return 0.0
+        }
+    }
+
+    fun getPrices(): List<PriceResponse> {
+        var list: List<PriceResponse> = listOf()
+        val binanceService = BinanceService.create()
+        var call: Call<PriceResponse>
+        var body: PriceResponse?
+        var priceResponse: PriceResponse
+        CryptoName.values().forEach { cryptoName ->
+            call = binanceService.getPrice(cryptoName.toString())
+            priceResponse = executeCall(cryptoName.toString(), call)
+            list += priceResponse
+        }
+        return list
     }
 }
