29c4d4f4fc94038e29aff9bba99587dd363214aa
fabriciomarote <fabrii.cai93@gmail.com>
2023-05-19 00:38:37 -0300

    Se crea CryptoCreateDTO.kt

--- src/main/kotlin/ar/edu/unq/desapp/grupoi202301/backenddesappapi/restWebService/DTO/CryptoCreateDTO.kt
+++ src/main/kotlin/ar/edu/unq/desapp/grupoi202301/backenddesappapi/restWebService/DTO/CryptoCreateDTO.kt
@@ -2,26 +2,27 @@ package ar.edu.unq.desapp.grupoi202301.backenddesappapi.restWebService.DTO
 
 import ar.edu.unq.desapp.grupoi202301.backenddesappapi.model.Crypto
 import ar.edu.unq.desapp.grupoi202301.backenddesappapi.model.CryptoName
+import java.time.LocalDateTime
 
-class CryptoSimpleDTO(
-    var id: Long?,
+class CryptoCreateDTO(
     var name: CryptoName?,
+    var time: LocalDateTime?,
     var price: Double? = null
 ) {
 
     fun toModel(): Crypto {
         val crypto = Crypto()
-        crypto.id = this.id
         crypto.name = this.name
+        crypto.time = this.time
         crypto.price = this.price
         return crypto
     }
 
     companion object {
         fun fromModel(crypto: Crypto) =
-            CryptoSimpleDTO(
-                id = crypto.id,
+            CryptoCreateDTO(
                 name = crypto.name,
+                time = crypto.time,
                 price = crypto.price,
             )
     }
