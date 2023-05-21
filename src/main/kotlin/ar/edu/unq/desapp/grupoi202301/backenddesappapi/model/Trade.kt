cd639ac63b7d3b8869801301b7fe15a631420645
fabriciomarote <fabrii.cai93@gmail.com>
2023-05-21 01:38:45 -0300

    Se agrega TODO por atributo quantity en Trade.kt

--- src/main/kotlin/ar/edu/unq/desapp/grupoi202301/backenddesappapi/model/Trade.kt
+++ src/main/kotlin/ar/edu/unq/desapp/grupoi202301/backenddesappapi/model/Trade.kt
@@ -15,10 +15,11 @@ class Trade {
     @NotNull(message = "The crypto cannot be null.")
     var crypto: Crypto? = null
 
-    @Column(nullable = false)
-    @NotNull(message = "The quantity cannot be null.")
-    @DecimalMin(value = "0.0", message = "The quantity cannot be negative.")
-    var quantity: Double? = null
+//    @Column(nullable = false)
+//    @NotNull(message = "The quantity cannot be null.")
+//    @DecimalMin(value = "0.0", message = "The quantity cannot be negative.")
+//    var quantity: Double? = null
+    // TODO: Revisar si quantity hace falta, cotizacion ya se encuentra en el crypto
 
     @Column(nullable = false)
     @NotNull(message = "The amount cannot be null.")
