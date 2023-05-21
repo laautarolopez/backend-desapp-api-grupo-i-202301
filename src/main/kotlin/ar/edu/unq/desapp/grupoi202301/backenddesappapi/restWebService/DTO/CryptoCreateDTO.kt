a357549aacbaf618caa44c2f593a6819eedf38fd
fabriciomarote <fabrii.cai93@gmail.com>
2023-05-21 01:35:14 -0300

    Se eliminan atributos a CryptoCreateDTO.kt

--- src/main/kotlin/ar/edu/unq/desapp/grupoi202301/backenddesappapi/restWebService/DTO/CryptoCreateDTO.kt
+++ src/main/kotlin/ar/edu/unq/desapp/grupoi202301/backenddesappapi/restWebService/DTO/CryptoCreateDTO.kt
@@ -2,19 +2,14 @@ package ar.edu.unq.desapp.grupoi202301.backenddesappapi.restWebService.DTO
 
 import ar.edu.unq.desapp.grupoi202301.backenddesappapi.model.Crypto
 import ar.edu.unq.desapp.grupoi202301.backenddesappapi.model.CryptoName
-import java.time.LocalDateTime
 
 class CryptoCreateDTO(
-    var name: CryptoName?,
-    var time: LocalDateTime?,
-    var price: Double? = null
+    var name: CryptoName?
 ) {
 
     fun toModel(): Crypto {
         val crypto = Crypto()
         crypto.name = this.name
-        crypto.time = this.time
-        crypto.price = this.price
         return crypto
     }
 
@@ -22,8 +17,6 @@ class CryptoCreateDTO(
         fun fromModel(crypto: Crypto) =
             CryptoCreateDTO(
                 name = crypto.name,
-                time = crypto.time,
-                price = crypto.price,
             )
     }
 }
