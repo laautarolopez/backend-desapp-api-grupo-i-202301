2eb8836446e6aae0cedc0da5ec3c6e157bc8fbac
Thomas Lautaro López <laautarolopez@gmail.com>
2023-05-22 20:45:29 -0300

    se agrego PriceDTO en CryptoDTO

--- src/main/kotlin/ar/edu/unq/desapp/grupoi202301/backenddesappapi/restWebService/DTO/CryptoDTO.kt
+++ src/main/kotlin/ar/edu/unq/desapp/grupoi202301/backenddesappapi/restWebService/DTO/CryptoDTO.kt
@@ -2,31 +2,30 @@ package ar.edu.unq.desapp.grupoi202301.backenddesappapi.restWebService.DTO
 
 import ar.edu.unq.desapp.grupoi202301.backenddesappapi.model.Crypto
 import ar.edu.unq.desapp.grupoi202301.backenddesappapi.model.CryptoName
+import ar.edu.unq.desapp.grupoi202301.backenddesappapi.restWebService.apiBinance.PriceResponse
 import java.time.LocalDateTime
 
 class CryptoDTO(
     var id: Long?,
     var name: CryptoName?,
-    var time: LocalDateTime?,
-    var price: Double? = null
+    var price: PriceDTO?
 ) {
 
     fun toModel(): Crypto {
         val crypto = Crypto()
         crypto.id = this.id
         crypto.name = this.name
-        crypto.time = this.time
-        crypto.price = this.price
         return crypto
     }
 
     companion object {
-        fun fromModel(crypto: Crypto) =
+        fun fromModel(crypto: Crypto) {
+            val cryptoPrice = crypto.getPrice()
             CryptoDTO(
                 id = crypto.id,
                 name = crypto.name,
-                time = crypto.time,
-                price = crypto.price,
+                price = PriceDTO(cryptoPrice.price, cryptoPrice.time)
             )
+        }
     }
 }
