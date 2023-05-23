9819d2b2a41ac7143a28266b0f9844ac13a90c95
Thomas Lautaro López <laautarolopez@gmail.com>
2023-05-23 17:31:53 -0300

    TransactionCreateDTO con idUserRequested e idTrade

--- src/main/kotlin/ar/edu/unq/desapp/grupoi202301/backenddesappapi/restWebService/DTO/TransactionCreateDTO.kt
+++ src/main/kotlin/ar/edu/unq/desapp/grupoi202301/backenddesappapi/restWebService/DTO/TransactionCreateDTO.kt
@@ -1,49 +1,19 @@
 package ar.edu.unq.desapp.grupoi202301.backenddesappapi.restWebService.DTO
 
-import ar.edu.unq.desapp.grupoi202301.backenddesappapi.model.ActionTransaction
 import ar.edu.unq.desapp.grupoi202301.backenddesappapi.model.Trade
 import ar.edu.unq.desapp.grupoi202301.backenddesappapi.model.Transaction
-import ar.edu.unq.desapp.grupoi202301.backenddesappapi.model.exceptions.ActionEmptyException
 
 class TransactionCreateDTO(
-    var idTrade: Long?,
-    var amountOperation: Double?,
-    var shippingAddress: String?,
-    var action: String?,
+    var idUserRequested: Long?,
+    var idTrade: Long?
 ) {
 
     fun toModel(): Transaction {
         val transaction = Transaction()
+        transaction.idUserRequested = idUserRequested
         val trade = Trade()
         trade.id = this.idTrade
         transaction.trade = trade
-        transaction.amountOperation = this.amountOperation
-        // TODO: agregar amount con llamado a la api?
-        transaction.shippingAddress = this.shippingAddress
-        transaction.action = this.verifyAction(this.action)
         return transaction
-    }
-
-    companion object {
-        fun fromModel(transaction: Transaction) =
-            TransactionCreateDTO(
-                idTrade = transaction.trade!!.id,
-                amountOperation = transaction.amountOperation,
-                shippingAddress = transaction.shippingAddress,
-                action = transaction.action.toString()
-            )
-    }
-
-    private fun verifyAction(action: String?): ActionTransaction {
-        var newAction: ActionTransaction
-        when(action) {
-            "MAKE" -> newAction = ActionTransaction.MAKE
-            "CONFIRM" -> newAction = ActionTransaction.CONFIRM
-            "CANCEL" -> newAction = ActionTransaction.CANCEL
-            else -> {
-                throw ActionEmptyException()
-            }
-        }
-        return newAction
     }
 }
