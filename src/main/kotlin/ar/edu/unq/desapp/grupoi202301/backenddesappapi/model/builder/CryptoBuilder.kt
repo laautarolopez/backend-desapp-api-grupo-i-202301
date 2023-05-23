48ad45d32d28eb66e5aa9b30793ff45b7a383019
fabriciomarote <fabrii.cai93@gmail.com>
2023-05-23 12:22:19 -0300

    Se eliminar atributos time y price en CryptoBuilder.kt

--- src/main/kotlin/ar/edu/unq/desapp/grupoi202301/backenddesappapi/model/builder/CryptoBuilder.kt
+++ src/main/kotlin/ar/edu/unq/desapp/grupoi202301/backenddesappapi/model/builder/CryptoBuilder.kt
@@ -2,12 +2,9 @@ package ar.edu.unq.desapp.grupoi202301.backenddesappapi.model.builder
 
 import ar.edu.unq.desapp.grupoi202301.backenddesappapi.model.Crypto
 import ar.edu.unq.desapp.grupoi202301.backenddesappapi.model.CryptoName
-import java.time.LocalDateTime
 
 class CryptoBuilder {
     var name: CryptoName? = null
-    var time: LocalDateTime? = null
-    var price: Double? = null
 
     fun build(): Crypto {
         var crypto = Crypto()
@@ -17,16 +14,6 @@ class CryptoBuilder {
 
     fun withName(name: CryptoName?): CryptoBuilder {
         this.name = name
-        return this
-    }
-
-    fun withTime(time: LocalDateTime?): CryptoBuilder {
-        this.time = time
-        return this
-    }
-
-    fun withPrice(price: Double?): CryptoBuilder {
-        this.price = price
         return this
     }
 }
