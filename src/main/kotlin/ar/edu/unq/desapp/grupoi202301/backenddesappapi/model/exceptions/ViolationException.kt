c7c990e4e6360f0616bdf168a5e917a8b7565e40
Thomas Lautaro López <laautarolopez@gmail.com>
2023-05-23 17:32:37 -0300

    se crea la clase ViolationException

---
+++ src/main/kotlin/ar/edu/unq/desapp/grupoi202301/backenddesappapi/model/exceptions/ViolationException.kt
@@ -0,0 +1,7 @@
+package ar.edu.unq.desapp.grupoi202301.backenddesappapi.model.exceptions
+
+open class ViolationException(val attribute: String, message: String) : RuntimeException(message)
+
+class TransactionException(attribute: String, message: String) : ViolationException(attribute, message)
+
+class TradeNonExistentException(attribute: String, message: String) : ViolationException(attribute, message)
