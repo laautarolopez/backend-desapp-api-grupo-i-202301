3ebdcd77a701642597443cedcc7696c6ddb710d2
Thomas Lautaro López <laautarolopez@gmail.com>
2023-05-23 14:04:36 -0300

    processTransaction en TransactionServiceImp(sin terminar)

--- src/main/kotlin/ar/edu/unq/desapp/grupoi202301/backenddesappapi/service/imp/TransactionServiceImp.kt
+++ src/main/kotlin/ar/edu/unq/desapp/grupoi202301/backenddesappapi/service/imp/TransactionServiceImp.kt
@@ -1,11 +1,14 @@
 package ar.edu.unq.desapp.grupoi202301.backenddesappapi.service.imp
 
+import ar.edu.unq.desapp.grupoi202301.backenddesappapi.model.ActionTransaction
+import ar.edu.unq.desapp.grupoi202301.backenddesappapi.model.OperationType
 import ar.edu.unq.desapp.grupoi202301.backenddesappapi.model.Transaction
 import ar.edu.unq.desapp.grupoi202301.backenddesappapi.persistence.TransactionPersistence
+import ar.edu.unq.desapp.grupoi202301.backenddesappapi.service.CryptoService
 import ar.edu.unq.desapp.grupoi202301.backenddesappapi.service.TradeService
 import ar.edu.unq.desapp.grupoi202301.backenddesappapi.service.TransactionService
 import jakarta.transaction.Transactional
-import org.springframework.data.repository.findByIdOrNull
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Service
 import org.springframework.validation.annotation.Validated
 
@@ -14,13 +17,17 @@ import org.springframework.validation.annotation.Validated
 @Transactional
 class TransactionServiceImp(
     private val transactionPersistence: TransactionPersistence,
-    private val tradeService: TradeService
+    @Autowired
+    private val tradeService: TradeService,
+    @Autowired
+    private val cryptoService: CryptoService
     ) : TransactionService {
 
     override fun create(transaction: Transaction): Transaction {
         recoverTrade(transaction)
         validateShippingAddress(transaction)
         validateAction(transaction)
+        processTransaction(transaction)
         return transactionPersistence.save(transaction)
     }
 
@@ -28,12 +35,12 @@ class TransactionServiceImp(
         transaction.trade = tradeService.getTrade(transaction.trade!!.id!!)
     }
 
-    override fun getTransaction(idTrade: Long): Transaction {
-        val transaction = transactionPersistence.findByIdOrNull(idTrade)
-        if (transaction == null) {
-            throw RuntimeException("The id does not exist.")
+    override fun getTransaction(idTransaction: Long): Transaction {
+        try {
+            return transactionPersistence.getReferenceById(idTransaction)
+        } catch(e: RuntimeException) {
+            throw RuntimeException("The transaction does not exist.")
         }
-        return transaction
     }
 
     override fun recoverAll(): List<Transaction> {
@@ -45,26 +52,54 @@ class TransactionServiceImp(
     }
 
     private fun validateShippingAddress(transaction: Transaction) {
-        if(transaction.trade!!.operation.toString() == "SALE") {
-           if(transaction.shippingAddress!!.length != 22) {
-               throw RuntimeException("The shipping address must contain a CVU with 22 digits.")
-           }
-        } else if(transaction.trade!!.operation.toString() == "BUY") {
-            if(transaction.shippingAddress!!.length != 8) {
-                throw RuntimeException("The shipping address must contain a walletAddress with 8 digits.")
-            }
+        if(isOperationType(transaction, OperationType.SALE)) {
+            transaction.shippingAddress = transaction.trade!!.user!!.cvuMercadoPago
+        } else if(isOperationType(transaction, OperationType.BUY)) {
+            transaction.shippingAddress = transaction.trade!!.user!!.walletAddress
         }
     }
 
+    private fun isOperationType(transaction: Transaction, operation: OperationType): Boolean {
+        return transaction.trade!!.operation == operation
+    }
+
     private fun validateAction(transaction: Transaction) {
-        if(transaction.action.toString() == "MAKE") {
-            if(transaction.trade!!.operation.toString() != "BUY") {
-                throw RuntimeException(".") // TODO: agregar mensaje de validacion
-            }
-        } else if(transaction.action.toString() == "CONFIRM") {
-            if(transaction.trade!!.operation.toString() != "SALE") {
-                throw RuntimeException(".") // TODO: agregar mensaje de validacion
-            }
+        // TODO: validar si el usuario de la transaccion es comprador o vendedor(no el user del trade)
+        if(isActionTransaction(transaction, ActionTransaction.CONFIRM) && isOperationType(transaction, OperationType.BUY)) {
+                throw RuntimeException("The action must be 'MAKE' or 'CANCEL' for buy operation.")
+        } else if(isActionTransaction(transaction, ActionTransaction.MAKE) && isOperationType(transaction, OperationType.SALE)) {
+            throw RuntimeException("The action must be 'CONFIRM' or 'CANCEL' for sale operation.")
+        }
+    }
+
+    private fun isActionTransaction(transaction: Transaction, action: ActionTransaction): Boolean {
+        return transaction.action == action
+    }
+
+    private fun processTransaction(transaction: Transaction) {
+        if(isSuccessful(transaction)) {
+
+        } else {
+
+        }
+    }
+
+    private fun isSuccessful(transaction: Transaction): Boolean {
+        return !isActionTransaction(transaction, ActionTransaction.CANCEL) && !isPriceDifferent(transaction)
+    }
+
+    private fun isPriceDifferent(transaction: Transaction): Boolean {
+        val crypto = transaction.trade!!.crypto
+        val tradePrice = transaction.trade!!.cryptoPrice
+        val actualPrice = cryptoService.getPrice(crypto!!.name.toString()).price
+        val maxVariation = tradePrice!! * 0.05
+
+        if(actualPrice < tradePrice) {
+            val variation = tradePrice - actualPrice
+            return variation > maxVariation
+        } else {
+            val variation = actualPrice - tradePrice
+            return variation > maxVariation
         }
     }
 }
