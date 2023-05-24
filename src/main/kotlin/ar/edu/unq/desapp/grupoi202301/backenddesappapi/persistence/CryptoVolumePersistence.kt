b9bd61fc7183dfbcb2085f854ccdde5ed3b09bf3
fabriciomarote <fabrii.cai93@gmail.com>
2023-05-24 18:14:44 -0300

    Se eliminan archivos

--- src/main/kotlin/ar/edu/unq/desapp/grupoi202301/backenddesappapi/persistence/CryptoVolumePersistence.kt
+++
@@ -1,8 +0,0 @@
-package ar.edu.unq.desapp.grupoi202301.backenddesappapi.persistence
-
-import ar.edu.unq.desapp.grupoi202301.backenddesappapi.model.CryptoVolume
-import org.springframework.data.jpa.repository.JpaRepository
-import org.springframework.stereotype.Repository
-
-@Repository
-interface CryptoVolumePersistence : JpaRepository<CryptoVolume, Long>
