be2f7bdf5c4733846734885c7ed36b92df2d34c7
fabriciomarote <fabrii.cai93@gmail.com>
2023-05-27 13:17:31 -0300

    Se termina de implementar funcionalidad getConfirmedTransactionsFromUserBetweenTwoDates en VolumeOperatedServiceImp.kt

--- src/main/kotlin/ar/edu/unq/desapp/grupoi202301/backenddesappapi/service/imp/VolumeOperatedServiceImp.kt
+++ src/main/kotlin/ar/edu/unq/desapp/grupoi202301/backenddesappapi/service/imp/VolumeOperatedServiceImp.kt
@@ -1,5 +1,7 @@
 package ar.edu.unq.desapp.grupoi202301.backenddesappapi.service.imp
 
+import ar.edu.unq.desapp.grupoi202301.backenddesappapi.model.CryptoOperated
+import ar.edu.unq.desapp.grupoi202301.backenddesappapi.model.TransactionStatus
 import ar.edu.unq.desapp.grupoi202301.backenddesappapi.model.User
 import ar.edu.unq.desapp.grupoi202301.backenddesappapi.model.Volume
 import ar.edu.unq.desapp.grupoi202301.backenddesappapi.service.*
@@ -20,17 +22,24 @@ class VolumeOperatedServiceImp(
 ) : VolumeOperatedService {
 
     override fun volumeOperatedByAUserBetweenDates(idUser: Long, firstDate: String, lastDate: String): Volume {
-        val user = validateYRecoverUserRequest(idUser)
+        validateYRecoverUserRequest(idUser)
         val date1 = LocalDateTime.parse(firstDate)
         val date2 = LocalDateTime.parse(lastDate)
         val requestDateTime = LocalDateTime.now()
-        var totalAmountUSD: Double
-        var totalAmountARS: Double
-        val transactionsConfirmed = transactionService.recoverConfirmed()
+        var totalAmountUSD: Double = 0.0
+        var totalAmountARS: Double = 0.0
+        val transactionsConfirmed = transactionService.recoverConfirmedTransactionsFromUserBetweenTwoDates(idUser, date1, date2, TransactionStatus.CONFIRMED)
 
+        transactionsConfirmed.forEach { transaction -> totalAmountUSD += transaction.getAmountUSD()!! }
+        transactionsConfirmed.forEach { transaction -> totalAmountARS += transaction.trade!!.amountARS!! }
 
-        TODO("Not yet implemented")
+        val cryptoOperateds = transactionsConfirmed.map { transaction -> CryptoOperated(transaction.trade!!.crypto!!.name!!,
+                                                                  transaction.trade!!.quantity!!,
+                                                                  transaction.trade!!.crypto!!.price!!,
+                                                                  transaction.trade!!.amountARS!!)}
 
+        var volumeResponse = Volume(idUser, requestDateTime, totalAmountUSD, totalAmountARS, cryptoOperateds)
+        return volumeResponse
     }
 
     private fun validateYRecoverUserRequest(idUser: Long?): User {
