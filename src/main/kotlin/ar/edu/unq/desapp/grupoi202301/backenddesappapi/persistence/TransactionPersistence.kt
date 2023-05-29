991976dd5402492a32502125c2ae4d484f9592cd
fabriciomarote <fabrii.cai93@gmail.com>
2023-05-29 20:15:35 -0300

    Se modifica Query en TransactionPersistence.kt

--- src/main/kotlin/ar/edu/unq/desapp/grupoi202301/backenddesappapi/persistence/TransactionPersistence.kt
+++ src/main/kotlin/ar/edu/unq/desapp/grupoi202301/backenddesappapi/persistence/TransactionPersistence.kt
@@ -4,14 +4,17 @@ import ar.edu.unq.desapp.grupoi202301.backenddesappapi.model.Transaction
 import ar.edu.unq.desapp.grupoi202301.backenddesappapi.model.TransactionStatus
 import org.springframework.data.jpa.repository.Query
 import org.springframework.data.repository.CrudRepository
+import org.springframework.data.repository.query.Param
 import org.springframework.stereotype.Repository
 import java.time.LocalDateTime
 
 @Repository
 interface TransactionPersistence : CrudRepository<Transaction, Long> {
 
-    @Query(value= "SELECT t FROM transaction t WHERE t.date BETWEEN ?2 AND ?3" +
-            "AND t.status = ?4 " +
-            "AND t.seller.id = ?1 OR t.buyer.id = ?1", nativeQuery = true)
-    fun getConfirmedTransactionsFromUserBetweenTwoDates(idUser: Long?, firstDate: LocalDateTime, lastDate: LocalDateTime, status: TransactionStatus?): List<Transaction>
+    @Query(value = "SELECT * FROM transactions t WHERE t.status = :status AND t.seller_id = :idUser OR t.buyer_id = :idUser " +
+            "AND t.date BETWEEN :firstDate AND :lastDate", nativeQuery = true)
+    fun getConfirmedTransactionsFromUserBetweenTwoDates(@Param("idUser")idUser: Long?,
+                                                        @Param("firstDate")firstDate: LocalDateTime,
+                                                        @Param("lastDate")lastDate: LocalDateTime,
+                                                        @Param("status")status: TransactionStatus?): List<Transaction>
 }
