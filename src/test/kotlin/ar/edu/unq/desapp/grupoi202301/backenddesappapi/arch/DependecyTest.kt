c13d5d1916ed74f51df78d49e8280e51f5e6bd3a
Thomas Lautaro López <laautarolopez@gmail.com>
2023-06-02 15:31:32 -0300

    DependecyTest created

---
+++ src/test/kotlin/ar/edu/unq/desapp/grupoi202301/backenddesappapi/arch/DependecyTest.kt
@@ -0,0 +1,65 @@
+package ar.edu.unq.desapp.grupoi202301.backenddesappapi.arch
+
+import com.tngtech.archunit.core.domain.JavaClasses
+import com.tngtech.archunit.core.importer.ClassFileImporter
+import com.tngtech.archunit.core.importer.ImportOption
+import com.tngtech.archunit.lang.syntax.ArchRuleDefinition.classes
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+
+class DependecyTest {
+    lateinit var baseClasses: JavaClasses
+
+    @BeforeEach
+    fun setup() {
+        baseClasses = ClassFileImporter()
+            .withImportOption(ImportOption.Predefined.DO_NOT_INCLUDE_TESTS)
+            .importPackages("ar.edu.unq.desapp.grupoi202301.backenddesappapi")
+    }
+
+    @Test
+    fun modelExceptionsClassesShouldEndWithException(){
+        classes().that().resideInAPackage("..model.exceptions..")
+            .should().haveSimpleNameEndingWith("Companion")
+            .orShould().haveSimpleNameEndingWith("Exception")
+            .check(baseClasses)
+    }
+
+    @Test
+    fun restWebServiceExceptionsClassesShouldContainExceptionOrError(){
+        classes().that().resideInAPackage("..restWebService.exception..")
+            .should().haveSimpleNameEndingWith("Companion")
+            .orShould().haveSimpleNameContaining("Exception")
+            .orShould().haveSimpleNameContaining("Error")
+            .check(baseClasses)
+    }
+
+    @Test
+    fun serviceExceptionsClassesShouldEndWithNonExistent(){
+        classes().that().resideInAPackage("..service..exception..")
+            .should().haveSimpleNameEndingWith("Companion")
+            .orShould().haveSimpleNameEndingWith("NonExistent")
+            .check(baseClasses)
+    }
+
+    @Test
+    fun persistenceClassesShouldEndWithPersistence(){
+        classes().that().resideInAPackage("..persistence..")
+            .should().haveSimpleNameEndingWith("Persistence")
+            .check(baseClasses)
+    }
+
+    @Test
+    fun serviceClassesShouldEndWithService(){
+        classes().that().resideInAPackage("..service")
+            .should().haveSimpleNameEndingWith("Service")
+            .check(baseClasses)
+    }
+
+    @Test
+    fun serviceImplementationClassesShouldEndWithServiceImp(){
+        classes().that().resideInAPackage("..service.imp")
+            .should().haveSimpleNameEndingWith("ServiceImp")
+            .check(baseClasses)
+    }
+}
