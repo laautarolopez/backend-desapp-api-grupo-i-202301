fc0b38e477d0e173feab7c5bbbfb7530e014d033
Thomas Lautaro López <laautarolopez@gmail.com>
2023-06-07 17:54:45 -0300

    dependecyTest renombrado a NameTest

--- src/test/kotlin/ar/edu/unq/desapp/grupoi202301/backenddesappapi/arch/NameTest.kt
+++ src/test/kotlin/ar/edu/unq/desapp/grupoi202301/backenddesappapi/arch/NameTest.kt
@@ -9,7 +9,7 @@ import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.TestInstance
 
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
-class DependecyTest {
+class NameTest {
     lateinit private var baseClasses: JavaClasses
 
     @BeforeAll
