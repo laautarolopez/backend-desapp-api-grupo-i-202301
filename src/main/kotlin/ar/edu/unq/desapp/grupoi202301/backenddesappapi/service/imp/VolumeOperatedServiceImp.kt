202955a27396300b93fd04ffbf458cb0531de310
fabriciomarote <fabrii.cai93@gmail.com>
2023-06-15 23:21:53 -0300

    Se finaliza implementacion del metodo para obtener el volumen operado de un usuario, se agregan mas validaciones

--- src/main/kotlin/ar/edu/unq/desapp/grupoi202301/backenddesappapi/service/imp/VolumeOperatedServiceImp.kt
+++ src/main/kotlin/ar/edu/unq/desapp/grupoi202301/backenddesappapi/service/imp/VolumeOperatedServiceImp.kt
@@ -1,7 +1,6 @@
 package ar.edu.unq.desapp.grupoi202301.backenddesappapi.service.imp
 
 import ar.edu.unq.desapp.grupoi202301.backenddesappapi.model.CryptoOperated
-import ar.edu.unq.desapp.grupoi202301.backenddesappapi.model.TransactionStatus
 import ar.edu.unq.desapp.grupoi202301.backenddesappapi.model.User
 import ar.edu.unq.desapp.grupoi202301.backenddesappapi.model.Volume
 import ar.edu.unq.desapp.grupoi202301.backenddesappapi.service.*
@@ -10,6 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Service
 import org.springframework.validation.annotation.Validated
 import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
 
 @Service
 @Validated
@@ -21,26 +21,38 @@ class VolumeOperatedServiceImp(
     private val userService: UserService,
 ) : VolumeOperatedService {
 
-    override fun volumeOperatedByAUserBetweenDates(idUser: Long, firstDate: LocalDateTime, lastDate: LocalDateTime): Volume {
-        validateYRecoverUserRequest(idUser)
+    override fun volumeOperatedByAUserBetweenDates(idUser: Long, firstDate: String, lastDate: String): Volume {
+        val user = validateYRecoverUserRequest(idUser)
+        validateDates(firstDate, lastDate)
         val requestDateTime = LocalDateTime.now()
         var totalAmountUSD: Double = 0.0
         var totalAmountARS: Double = 0.0
-        val transactionsConfirmed = transactionService.recoverConfirmedTransactionsFromUserBetweenTwoDates(idUser, firstDate, lastDate, TransactionStatus.CONFIRMED)
+        val formatter: DateTimeFormatter = DateTimeFormatter.ISO_LOCAL_DATE_TIME
+        val date1 = LocalDateTime.parse(firstDate, formatter)
+        val date2 = LocalDateTime.parse(lastDate, formatter)
+        val transactionsConfirmed = transactionService.recoverConfirmedTransactionsFromUserBetweenTwoDates(user.id!!, date1, date2)
 
         transactionsConfirmed.forEach { transaction -> totalAmountUSD += transaction.getAmountUSD()!! }
         transactionsConfirmed.forEach { transaction -> totalAmountARS += transaction.trade!!.amountARS!! }
 
-        val cryptoOperateds = transactionsConfirmed.map { transaction -> CryptoOperated(transaction.trade!!.crypto!!.name!!,
+        val cryptoOperateds = transactionsConfirmed.map { transaction -> CryptoOperated(
+                                                                  transaction.trade!!.crypto!!.id!!,
+                                                                  transaction.trade!!.crypto!!.name!!,
                                                                   transaction.trade!!.quantity!!,
                                                                   transaction.trade!!.crypto!!.price!!,
                                                                   transaction.trade!!.amountARS!!)}
 
-        var volumeResponse = Volume(idUser, requestDateTime, totalAmountUSD, totalAmountARS, cryptoOperateds)
+        var volumeResponse = Volume(user.id!!.toLong(), requestDateTime, totalAmountUSD, totalAmountARS, cryptoOperateds)
         return volumeResponse
     }
 
     private fun validateYRecoverUserRequest(idUser: Long?): User {
         return userService.getUser(idUser)
+    }
+
+    private fun validateDates(date1: String?, date2: String?) {
+        if (date1 == "" || date2 == "") {
+            throw RuntimeException("At least one date is invalid.")
+        }
     }
 }
