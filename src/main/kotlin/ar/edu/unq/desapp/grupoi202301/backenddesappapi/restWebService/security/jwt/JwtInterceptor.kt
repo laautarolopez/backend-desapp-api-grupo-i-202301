6aee838e59d8480f519956a5e41f3271382afbc8
Thomas Lautaro López <laautarolopez@gmail.com>
2023-06-22 19:53:25 -0300

    RoleInterceptor para corroborar el rol admin

--- src/main/kotlin/ar/edu/unq/desapp/grupoi202301/backenddesappapi/restWebService/security/jwt/JwtInterceptor.kt
+++ src/main/kotlin/ar/edu/unq/desapp/grupoi202301/backenddesappapi/restWebService/security/jwt/JwtInterceptor.kt
@@ -19,9 +19,6 @@ class JwtInterceptor : HandlerInterceptor {
         if (token != null && jwtUtil.validateToken(token)) {
             val email = jwtUtil.getEmailFromToken(token)
             if (email != null) {
-                // Realiza la lógica de autenticación, por ejemplo, establecer el contexto de seguridad
-                // o realizar verificaciones adicionales según tus necesidades
-                // Aquí se muestra un ejemplo básico de cómo establecer el contexto de seguridad:
                 val authentication = UsernamePasswordAuthenticationToken(email, null, emptyList())
                 SecurityContextHolder.getContext().authentication = authentication
                 return true
@@ -31,11 +28,37 @@ class JwtInterceptor : HandlerInterceptor {
         return false
     }
 
-    private fun extractToken(request: HttpServletRequest): String? {
+    fun extractToken(request: HttpServletRequest): String? {
         val bearerToken = request.getHeader("Authorization")
         if (bearerToken != null && bearerToken.startsWith("Bearer ")) {
             return bearerToken.substring(7)
         }
         return null
+    }
+}
+
+@Component
+class RoleInterceptor : JwtInterceptor() {
+
+    @Autowired
+    private lateinit var jwtUtil: JwtUtil
+
+    override fun preHandle(request: HttpServletRequest, response: HttpServletResponse, handler: Any): Boolean {
+        val token = extractToken(request)
+
+        if (token != null && jwtUtil.validateToken(token)) {
+            val role = jwtUtil.getRoleFromToken(token)
+            if(role == "ADMIN") {
+                val email = jwtUtil.getEmailFromToken(token)
+                if (email != null) {
+                    val authentication = UsernamePasswordAuthenticationToken(email, null, emptyList())
+                    SecurityContextHolder.getContext().authentication = authentication
+                    return true
+                }
+            }
+        }
+
+        response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "Token inválido o expirado")
+        return false
     }
 }
