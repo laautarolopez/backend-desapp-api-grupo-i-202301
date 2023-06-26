0283e668eb91d99e24ae0ec766ad7e2076b2a856
Thomas Lautaro López <laautarolopez@gmail.com>
2023-06-26 17:51:50 -0300

    findByCryptoName en QuotePersistence

--- src/main/kotlin/ar/edu/unq/desapp/grupoi202301/backenddesappapi/persistence/QuotePersistence.kt
+++ src/main/kotlin/ar/edu/unq/desapp/grupoi202301/backenddesappapi/persistence/QuotePersistence.kt
@@ -1,8 +1,13 @@
 package ar.edu.unq.desapp.grupoi202301.backenddesappapi.persistence
 
+import ar.edu.unq.desapp.grupoi202301.backenddesappapi.model.CryptoName
 import ar.edu.unq.desapp.grupoi202301.backenddesappapi.model.Quote
+import ar.edu.unq.desapp.grupoi202301.backenddesappapi.model.User
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.stereotype.Repository
 
 @Repository
-interface QuotePersistence: JpaRepository<Quote, Long>
+interface QuotePersistence: JpaRepository<Quote, Long> {
+
+    fun findByCryptoName(cryptoName: CryptoName): Quote
+}
