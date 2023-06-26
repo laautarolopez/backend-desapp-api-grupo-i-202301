885b062473b018555176035c57b17a9b4f0fa17e
Thomas Lautaro López <laautarolopez@gmail.com>
2023-06-26 15:58:52 -0300

    clase QuoteNonExistentException creada

--- src/main/kotlin/ar/edu/unq/desapp/grupoi202301/backenddesappapi/model/exceptions/ViolationException.kt
+++ src/main/kotlin/ar/edu/unq/desapp/grupoi202301/backenddesappapi/model/exceptions/ViolationException.kt
@@ -5,5 +5,6 @@ open class ViolationException(val attribute: String, message: String) : RuntimeE
 class TransactionException(attribute: String, message: String) : ViolationException(attribute, message)
 
 class TradeNonExistentException(attribute: String, message: String) : ViolationException(attribute, message)
+class QuoteNonExistentException(attribute: String, message: String) : ViolationException(attribute, message)
 
 class UnauthorizedException(val attribute: String, message: String): RuntimeException(message)
