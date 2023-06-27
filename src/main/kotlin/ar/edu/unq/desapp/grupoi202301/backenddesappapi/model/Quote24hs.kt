3d7a5e8ad170adf251d2dcf6df49b9a6e495a5c8
Thomas Lautaro López <laautarolopez@gmail.com>
2023-06-27 13:07:26 -0300

    @JsonIgnore en id de Quote

---
+++ src/main/kotlin/ar/edu/unq/desapp/grupoi202301/backenddesappapi/model/Quote24hs.kt
@@ -0,0 +1,25 @@
+package ar.edu.unq.desapp.grupoi202301.backenddesappapi.model
+
+import com.fasterxml.jackson.annotation.JsonIgnore
+import jakarta.persistence.*
+import jakarta.validation.constraints.DecimalMin
+import jakarta.validation.constraints.NotNull
+
+@Entity(name = "quotes24hs")
+class Quote24hs {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @JsonIgnore
+    var id: Long? = null
+
+    @Column(nullable = false)
+    @NotNull(message = "The cryptoname cannot be null.")
+    var cryptoName: CryptoName? = null
+
+    @Column
+    @DecimalMin(value = "0.0", message = "The price cannot be negative.")
+    var price: Double? = null
+
+    @Column
+    var time: String? = null
+}
